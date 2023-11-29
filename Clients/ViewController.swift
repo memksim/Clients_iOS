bb415da29eb989485fc535cce15ec12f220ce14f
Maksim Kosenko <maksim.kosenko@rosbank.ru>
2023-11-29 17:50:49 +0500

    Initial Commit

---
+++ Clients/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clients
+//
+//  Created by Максим Косенко on 29.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
